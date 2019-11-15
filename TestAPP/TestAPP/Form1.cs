8c6bd5bb1c93ea9083f00df6b1da14ea6b86a17e
piotrochendusz <piotrochendusz@gmail.com>
2019-11-15 13:02:35 +0100

    to revert

--- TestAPP/TestAPP/Form1.cs
+++ TestAPP/TestAPP/Form1.cs
@@ -26,6 +26,7 @@ namespace TestAPP
             label2.Text = "initComEDITED WITH AMEND 1 2";
             label3.Text = "changes from master"; //test
             label1.Text = "master changes";
+            //revert
         }
     }
 }
