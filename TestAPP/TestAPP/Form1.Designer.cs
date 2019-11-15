99b25ab3c0aedf5a4598837b840662b7bb6c36a9
piotrochendusz <piotrochendusz@gmail.com>
2019-11-15 08:44:13 +0100

    label 1 2 3  set  to left

--- TestAPP/TestAPP/Form1.Designer.cs
+++ TestAPP/TestAPP/Form1.Designer.cs
@@ -48,7 +48,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 122);
+            this.label1.Location = new System.Drawing.Point(12, 123);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 1;
@@ -66,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 216);
+            this.label2.Location = new System.Drawing.Point(12, 216);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 2;
@@ -75,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(61, 249);
+            this.label3.Location = new System.Drawing.Point(12, 249);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 3;
